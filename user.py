cbdb1926124f9fe5ef788b20401306d5b986a166
MakaelaRdz <100048409+MakaelaRdz@users.noreply.github.com>
2022-05-13 13:31:51 -0700

    commit

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+
+# chaining method isn't working so we need to figuer out why! user bank account 
+
 class User:
 
     def __init__(self, name, email):
@@ -7,11 +10,22 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdraw (self, amount):
         self.account_balance -= amount
+        return self
 
-    def display_user_balance(self):
+    def display_user_balance(self, amount):
         print(f"{self.name}, balance: {self.account_balance}")
+        return self
+        
+    # def make_transfer (self, amount)
+        # self.make_transfer -= amount
+
+
 user1 = User("John", "John@dojo.com")
 user2 = User("Greg", "Greg@ninja.com")
+
+
+# chaining method isn't working so we need to figuer out why!
